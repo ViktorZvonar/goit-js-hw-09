06dc26f47490d92f80695d67457beac9ef288271
ViktorZvonar <viktorzvonar@yahoo.com>
2022-11-22 16:03:14 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+console.log(startBtn);
+const stopBtn = document.querySelector('button[data-stop]');
+console.log(stopBtn);
+let intervalId = null;
+
+startBtn.addEventListener('click', changeBodyColor);
+stopBtn.addEventListener('click', stopChangeBodyColor);
+
+function changeBodyColor() {
+  startBtn.setAttribute('disabled', true);
+  intervalId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function stopChangeBodyColor() {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled');
+}
